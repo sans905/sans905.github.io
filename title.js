534f362943f0cc13927ff1f1052e197d03b7a6bc
$an$ <124837779+sans905@users.noreply.github.com>
2023-03-22 13:09:44 -0700

    bruh

--- title.js
+++
@@ -1,5 +0,0 @@
-if (localStorage.getItem("savedTitle") = null) {
-    localStorage.setItem("savedTitle", orgTitle)
-}
-var thistitle = localStorage.getItem("savedTitle");
-document.title = thistitle;
