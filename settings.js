f6ea374463ec1b5102e8580f19ac3f37e2a4eb57
$an$ <124837779+sans905@users.noreply.github.com>
2023-03-31 15:14:55 -0700

    fx

--- settings.js
+++ settings.js
@@ -1,6 +1,3 @@
 function setTitle() {
     var titleinput = document.getElementById("titleinput").value;
-    document.title = titleinput;
-    localStorage.removeItem("title");
-    localStorage.addItem("title", titleinput);
 }
