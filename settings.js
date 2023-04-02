0a4b4e7c46aca275857897e443e3b34c4237915e
$an$ <124837779+sans905@users.noreply.github.com>
2023-04-02 14:43:16 -0700

    fix

--- settings.js
+++ settings.js
@@ -1,3 +1,14 @@
 function setTitle() {
-    var titleinput = document.getElementById("titleinput").value;
+    var titleinputperson = document.getElementById("titleinput").value;
+    document.title = titleinputperson;
+    if (titleinputperson == "") {
+        window.localStorage.addItem("titled", titleinputperson);
+    } else {
+        window.localStorage.setItem("titled", titleinputperson);
+    }
 }
+var titlecheck = window.localStorage.getItem("titled");
+function test() {
+    alert(titlecheck);
+}
+document.title = titlecheck;
